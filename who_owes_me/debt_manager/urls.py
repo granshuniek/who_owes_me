74dff4f4d45274888f7b684add92119d587cf1a8
granshuniek <granshuniek@gmail.com>
2018-03-29 16:20:29 +0200

    changed name in urls and base html

--- who_owes_me/debt_manager/urls.py
+++ who_owes_me/debt_manager/urls.py
@@ -4,12 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 
-    path('debts/', views.DebtListView.as_view(), name='debts'),
-    path('debt/<int:pk>', views.DebtDetailView.as_view(), name='debt-detail'),
+    path('debts/', views.DebtListView.as_view(), name='debts-list'),
+    path('debt/<int:pk>', views.DebtDetailView.as_view(), name='debts-detail'),
 
-    path('debtors/', views.DebtorListView.as_view(), name='debtors'),
-    path('debtor/<int:pk>', views.DebtorDetailView.as_view(), name='debtor-detail'),
+    path('debtors/', views.DebtorListView.as_view(), name='debtors-list'),
+    path('debtor/<int:pk>', views.DebtorDetailView.as_view(), name='debtors-detail'),
 
-    path('creditors/', views.CreditorListView.as_view(), name='creditors'),
-    path('reditor/<int:pk>', views.CreditorDetailView.as_view(), name='creditor-detail'),
+    path('creditors/', views.CreditorListView.as_view(), name='creditors-list'),
+    path('reditor/<int:pk>', views.CreditorDetailView.as_view(), name='creditors-detail'),
 ]
