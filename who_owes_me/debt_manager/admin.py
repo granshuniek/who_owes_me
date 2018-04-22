ed68bab27df3fc7df131d517446635025d7927ac
granshuniek <granshuniek@gmail.com>
2018-04-22 02:55:16 +0200

    delete doubled line in admin module

--- who_owes_me/debt_manager/admin.py
+++ who_owes_me/debt_manager/admin.py
@@ -4,5 +4,4 @@ from .models import Debtors, Debts, Creditors, User
 admin.site.register(Debtors)
 admin.site.register(Creditors)
 admin.site.register(Debts)
-admin.site.register(Debts)
 admin.site.register(User, UserAdmin)
