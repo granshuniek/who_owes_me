45fe2b5fb1a1b09d7b7e7d87e9e92f1fdd79e8c3
granshuniek <granshuniek@gmail.com>
2018-04-23 20:06:18 +0200

    Deleted unused receivers from Profile model

--- who_owes_me/debt_manager/models.py
+++ who_owes_me/debt_manager/models.py
@@ -29,15 +29,6 @@ class Profile(models.Model):
             self.avatar = saved_image
         super(Profile, self).save(*args, **kwargs)
 
-    # @receiver(post_save, sender=User)
-    # def create_user_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         Profile.objects.create(user=instance)
-    
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.profile.save()
-
 class Debtors(models.Model):
     """
         Model represents people who are owe money.
