afa06d00ce2a34db167b718f48d4f6fd43c421ad
granshuniek <granshuniek@gmail.com>
2018-06-17 15:27:47 +0200

    Fix of debts list manager

--- who_owes_me/debt_manager/managers.py
+++ who_owes_me/debt_manager/managers.py
@@ -55,11 +55,13 @@ class CreditorsAndDebtorsManager(models.Manager):
 class DebtsManager(models.Manager):
 
     def get_current_user_dict_of_debts(self, users_id):
+        debtor_id = _get_debtor_or_creditor_id(DEBTOR, users_id)
         sql = '''
             SELECT * from debt_manager_debts where debtor_id={0}
-        '''.format(users_id)
+        '''.format(debtor_id)
         users_debt_list = []
         response = _execute_sql(sql)
+
         for row in response:
             debt_dict = {}
             debt_dict['id'] = row[0]
@@ -67,9 +69,21 @@ class DebtsManager(models.Manager):
             debt_dict['for_what'] = row[2]
             debt_dict['date'] = row[4]
             creditor_tuple = _get_creditor_or_debtor_info(CREDITOR, row[5])
+            print(creditor_tuple)
             debt_dict['creditor'] = "{0} {1}".format(creditor_tuple[0], creditor_tuple[1])
             users_debt_list.append(debt_dict)
         return users_debt_list
+
+def _get_debtor_or_creditor_id(person_type, user_id):
+    user_profile_sql = 'SELECT id FROM debt_manager_profile WHERE user_id={0}'.format(user_id)
+    user_profile_id = _execute_sql(user_profile_sql)[0][0]
+    if person_type == CREDITOR:
+        creditor_sql = 'SELECT id FROM debt_manager_creditors WHERE user_id={0}'.format(user_profile_id)
+        person_id = _execute_sql(creditor_sql)[0][0]
+    elif person_type == DEBTOR:
+        debtor_sql = 'SELECT id FROM debt_manager_debtors WHERE user_id={0}'.format(user_profile_id)
+        person_id = _execute_sql(debtor_sql)[0][0]
+    return person_id
 
 def _get_creditor_or_debtor_info(person_type, person_id):
     if person_type == CREDITOR:
@@ -84,4 +98,4 @@ def _get_creditor_or_debtor_info(person_type, person_id):
     auth_user_info_sql = 'SELECT first_name, last_name, username FROM auth_user WHERE id={0}'.format(auth_user_id)
     auht_user_info = _execute_sql(auth_user_info_sql)[0]
 
-    return auth_user_info_sql
+    return auht_user_info
