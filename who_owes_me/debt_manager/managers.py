3bccbb0bd4d4ec11b130df84f78d34b93134f6a8
granshuniek <granshuniek@gmail.com>
2018-06-19 01:52:54 +0200

    Debtors are taken from db, but they are not display properly

--- who_owes_me/debt_manager/managers.py
+++ who_owes_me/debt_manager/managers.py
@@ -8,55 +8,21 @@ def _execute_sql(sql):
         cursor.execute(sql)
         return cursor.fetchall()
 
+class DebtorsManager(models.Manager):
+    def get_current_user_debtors(self, users_id):
+        user_as_creditor_id = _get_debtor_or_creditor_id(CREDITOR, users_id)
+        sql = '''
+            SELECT debtor_id FROM debt_manager_debts WHERE creditor_id={0}
+        '''.format(user_as_creditor_id)
+        debtors_ids_list = _execute_sql(sql)
+        debtors_list = []
+        for debtors_id in debtors_ids_list:
+            debtor_info_tuple = _get_creditor_or_debtor_info(DEBTOR, debtors_id[0])
+            debtors_list.append('{0} {1}'.format(debtor_info_tuple[0], debtor_info_tuple[1]))
+        return debtors_list
+
 class CreditorsManager(models.Manager):
     pass
-
-class DebtorsManager(models.Manager):
-    pass
-
-class CreditorsAndDebtorsManager(models.Manager):
-    """
-        Manager for Debtors and Creditors models.
-    """
-    def get_creditor_charges_sum(self):
-        """
-        Method sum amount of charges of current creditor.
-        
-        :returns: return_dict
-            { creditor_id: sumed_charge }
-        """
-
-        sql = '''
-            SELECT crds.id, sum(dts.amount)
-            FROM debt_manager_debts dts
-            JOIN debt_manager_creditors crds on crds.id=dts.creditor_id
-            GROUP BY crds.id
-        '''
-        response = _execute_sql(sql)
-        return_dict = {}
-        for creditor_id, sumed_charge in response:
-            return_dict[creditor_id] = sumed_charge
-        return return_dict    
-
-    def get_debtor_debts_sum(self):
-        """
-        Method sum amount of debts of current debtor.
-        
-        :returns: return_dict
-            { debtor_id: sumed_debt }
-        """
-
-        sql = '''
-            SELECT dbtr.id, sum(dts.amount)
-            FROM debt_manager_debts dts
-            JOIN debt_manager_debtors dbtr on dbtr.id=dts.debtor_id
-            GROUP BY dbtr.id
-        '''
-        response = _execute_sql(sql)
-        return_dict = {}
-        for debtor_id, sumed_debt in response:
-            return_dict[debtor_id] = sumed_debt
-        return return_dict
 
 class DebtsManager(models.Manager):
 
