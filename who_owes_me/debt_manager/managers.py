a5fc87fec2a92c10d1d9221a0b37f368b25f34b9
Damian Korzeniowski <damiankorzeniowski@gmail.com>
2018-06-18 16:10:22 +0200

    Second way to resolve creditor or debtor info getter

--- who_owes_me/debt_manager/managers.py
+++ who_owes_me/debt_manager/managers.py
@@ -8,6 +8,12 @@ def _execute_sql(sql):
             cursor.execute(sql)
             return cursor.fetchall()
 
+class CreditorsManager(models.Manager):
+    pass
+
+class DebtorsManager(models.Manager):
+    pass
+
 class CreditorsAndDebtorsManager(models.Manager):
     """
         Manager for Debtors and Creditors models.
@@ -72,9 +78,16 @@ class DebtsManager(models.Manager):
         return users_debt_list
 
 def _get_creditor_or_debtor_info(person_type, creditor_id):
-    user_profile_sql = 'SELECT user_id FROM debt_manager_creditors WHERE id={0}'.format(creditor_id)
-    user_profile_id = _execute_sql(user_profile_sql)[0][0]
+    if person_type == CREDITOR:
+        user_profile_sql = 'SELECT user_id FROM debt_manager_creditors WHERE id={0}'.format(creditor_id)
+        user_profile_id = _execute_sql(user_profile_sql)[0][0]
+    if person_type == DEBTOR:
+        user_profile_sql = 'SELECT user_id FROM debt_manager_debtors WHERE id={0}'.format(creditor_id)
+        user_profile_id = _execute_sql(user_profile_sql)[0][0]
+
     aut_user_sql = 'SELECT user_id FROM debt_manager_profile WHERE id={0}'.format(user_profile_id)
     auth_user_id = _execute_sql(aut_user_sql)[0][0]
     auth_user_info_sql = 'SELECT first_name, last_name, username FROM auth_user WHERE id={0}'.format(auth_user_id)
-    return _execute_sql(auth_user_info_sql)[0]
+    user_info = _execute_sql(auth_user_info_sql)[0]
+
+    return user_info
